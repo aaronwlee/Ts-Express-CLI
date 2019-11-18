fdd85c5e1a950a12bff43a7964c30d01a0370734
aaronwlee <aaron.lee@cannalogue.ca>
2019-11-18 01:20:42 -0500

    init test

--- dist/lib/initializer.js
+++ dist/lib/initializer.js
@@ -6,7 +6,45 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const shelljs_1 = __importDefault(require("shelljs"));
 function initializer(projectName) {
     shelljs_1.default.mkdir(`${process.cwd()}/${projectName}`);
-    shelljs_1.default.exec(`${process.cwd()}/${projectName}/ npm init -y`);
+    shelljs_1.default.cd(`${projectName}`);
+    shelljs_1.default.echo(packagejsString(projectName)).to(`${process.cwd()}/${projectName}/package.json`);
 }
+const packagejsString = (projectName) => `{
+  "name": "${projectName}",
+  "version": "1.0.0",
+  "main": "index.js",
+  "license": "MIT",
+  "bin": {
+    "tx": "./dist/bin/index.js"
+  },
+  "scripts": {
+    "build": "tsc",
+    "start": "tsc -w | nodemon dist/server.js"
+  },
+  "devDependencies": {
+    "@types/inquirer": "^6.5.0",
+    "@types/shelljs": "^0.8.6",
+    "@types/node": "^12.12.8",
+    "@types/express": "^4.17.2",
+    "@types/mongoose": "^5.5.32",
+    "@types/compression": "^0.0.36",
+    "@types/helmet": "^0.0.43",
+    "@types/dotenv": "^6.1.1",
+    "@types/cors": "^2.8.5",
+    "ts-node": "^8.5.0",
+    "tsc": "^1.20150623.0",
+    "typescript": "^3.7.2"
+  },
+  "dependencies": {
+    "mongo-to-gql": "^2.0.2",
+    "mongoose": "^5.7.11",
+    "express": "^4.17.1",
+    "cors": "^2.8.5",
+    "dotenv": "^8.0.0",
+    "compression": "^1.7.4",
+    "helmet": "^3.20.0",
+  }
+}
+`;
 exports.default = initializer;
 //# sourceMappingURL=initializer.js.map
--- src/lib/initializer.ts
+++ src/lib/initializer.ts
@@ -1,9 +1,48 @@
 import shell from 'shelljs'
 
 function initializer(projectName: string) {
-    shell.mkdir(`${process.cwd()}/${projectName}`)
-    shell.cd(`${projectName}`)
-    shell.exec('npm init -y')
+  shell.mkdir(`${process.cwd()}/${projectName}`)
+  shell.cd(`${projectName}`)
+  shell.echo(packagejsString(projectName)).to(`${process.cwd()}/${projectName}/package.json`)
 }
+
+const packagejsString = (projectName: string) =>
+`{
+  "name": "${projectName}",
+  "version": "1.0.0",
+  "main": "index.js",
+  "license": "MIT",
+  "bin": {
+    "tx": "./dist/bin/index.js"
+  },
+  "scripts": {
+    "build": "tsc",
+    "start": "tsc -w | nodemon dist/server.js"
+  },
+  "devDependencies": {
+    "@types/inquirer": "^6.5.0",
+    "@types/shelljs": "^0.8.6",
+    "@types/node": "^12.12.8",
+    "@types/express": "^4.17.2",
+    "@types/mongoose": "^5.5.32",
+    "@types/compression": "^0.0.36",
+    "@types/helmet": "^0.0.43",
+    "@types/dotenv": "^6.1.1",
+    "@types/cors": "^2.8.5",
+    "ts-node": "^8.5.0",
+    "tsc": "^1.20150623.0",
+    "typescript": "^3.7.2"
+  },
+  "dependencies": {
+    "mongo-to-gql": "^2.0.2",
+    "mongoose": "^5.7.11",
+    "express": "^4.17.1",
+    "cors": "^2.8.5",
+    "dotenv": "^8.0.0",
+    "compression": "^1.7.4",
+    "helmet": "^3.20.0",
+  }
+}
+`
 
 export default initializer
