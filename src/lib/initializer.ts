3db3375c8d10d8bee57c15e2d5c3f11f443d2dff
aaronwlee <aaron.lee@cannalogue.ca>
2019-11-18 01:09:22 -0500

    init test

--- src/lib/initializer.ts
+++ src/lib/initializer.ts
@@ -2,7 +2,8 @@ import shell from 'shelljs'
 
 function initializer(projectName: string) {
     shell.mkdir(`${process.cwd()}/${projectName}`)
-    shell.exec(`${process.cwd()}/${projectName}/ npm init -y`)
+    shell.cd(`${projectName}`)
+    shell.exec('npm init -y')
 }
 
 export default initializer
