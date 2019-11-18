0b4e8aa4cbb1f34667140fb64cbb59304fc9b47e
aaronwlee <aaron.lee@cannalogue.ca>
2019-11-18 01:05:36 -0500

    init test

--- dist/lib/initializer.js
+++ dist/lib/initializer.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const shelljs_1 = __importDefault(require("shelljs"));
 function initializer(projectName) {
-    shelljs_1.default.mkdir(`./${projectName}`);
-    shelljs_1.default.exec(`./${projectName} npm init -y`);
+    shelljs_1.default.mkdir(`${process.cwd()}/${projectName}`);
+    shelljs_1.default.exec(`${process.cwd()}/${projectName} npm init -y`);
 }
 exports.default = initializer;
 //# sourceMappingURL=initializer.js.map
--- src/lib/initializer.ts
+++ src/lib/initializer.ts
@@ -1,8 +1,8 @@
 import shell from 'shelljs'
 
 function initializer(projectName: string) {
-    shell.mkdir(`./${projectName}`)
-    shell.exec(`./${projectName} npm init -y`)
+    shell.mkdir(`${process.cwd()}/${projectName}`)
+    shell.exec(`${process.cwd()}/${projectName} npm init -y`)
 }
 
 export default initializer
